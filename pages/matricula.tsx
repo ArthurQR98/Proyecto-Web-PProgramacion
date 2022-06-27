4c5d1063348ba1e986bdfdc227a29b240b9ffd84
ArthurQRPE98 <arthur.quezada98@gmail.com>
2022-06-26 21:44:14 -0500

    Agrego el concepto de como ingresa estilos

--- pages/matricula.tsx
+++ pages/matricula.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Text } from '@geist-ui/react';
 
 const Page = () => (
-  <Text h2 style={{ margin: '8rem 0', textAlign: 'center' }}>
+  // Para los componentes que tenga por defecto soporte la libreria (geist) se ingresa el css en el componente
+  // por el prop llamado "style"
+  // y para lo que no (div, table, ul, etc) esos tipos no tienen soporte por parte de libreria el ingreso
+  // de css se ingresa con la declarativa <style jsx>`{.claseCss{atributos}}`</style>
+  <Text h2 className="center" style={{ margin: '8rem 0', textAlign: 'center' }}>
     Matricula
   </Text>
 );
